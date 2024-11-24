c6965dd8106dc8619d2d77d7205b6dbfbbe67b48
MaheswaranPalaniselvan <79225498+MaheswaranPalaniselvan@users.noreply.github.com>
2024-11-24 21:59:47 +0530

    Update send_msg_to_tele.py

--- send_msg_to_tele.py
+++ send_msg_to_tele.py
@@ -14,5 +14,5 @@ def send_msg(msg: str):
     url = f"https://api.telegram.org/bot{bot_id}/sendMessage?chat_id={chat_id}&text="
     _ = requests.get(url+msg)
     
-if __name__ == "__main__:
+if __name__ == "__main__":
     send_msg("test now!!")
