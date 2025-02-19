c418b129858b617f7be9d5f0a4d789362c432880
MaheswaranPalaniselvan <79225498+MaheswaranPalaniselvan@users.noreply.github.com>
2025-02-20 00:22:35 +0530

    Create xts_docs.py

---
+++ stock_brokers/xts_docs.py
@@ -0,0 +1,6 @@
+# https://github.com/symphonyfintech/xts-interactive-api
+
+# https://github.com/symphonyfintech/xts-pythonclient-api-sdk/blob/main/Example.py
+
+# https://symphonyfintech.com/xts-trading-front-end-api-v2/
+# https://symphonyfintech.com/xts-market-data-front-end-api-v2/
